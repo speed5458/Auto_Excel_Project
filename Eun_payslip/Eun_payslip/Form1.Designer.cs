491f455dea7993b31b4119eb208997bb781a2c2e
speed6548 <speed5458@gmail.com>
2020-02-12 09:44:54 +0900

    본인부담금파일 읽어오기, 개인 파일 다중 입력 기능 완료.

---
+++ Eun_payslip/Eun_payslip/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Eun_payslip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.ofdFile2 = new System.Windows.Forms.OpenFileDialog();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblperiod = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Location = new System.Drawing.Point(0, 131);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(345, 325);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.listView1);
+            this.panel6.Location = new System.Drawing.Point(3, 62);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(337, 258);
+            this.panel6.TabIndex = 11;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(337, 258);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.Controls.Add(this.button1);
+            this.panel5.Controls.Add(this.textBox1);
+            this.panel5.Location = new System.Drawing.Point(3, 11);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(337, 45);
+            this.panel5.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(112, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "파일이름";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "파일추가";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(198, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 25);
+            this.textBox1.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.lblperiod);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(825, 112);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.simpleButton1);
+            this.panel4.Location = new System.Drawing.Point(351, 131);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(460, 325);
+            this.panel4.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(129, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Header";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(129, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Header";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.panel7);
+            this.panel2.Location = new System.Drawing.Point(0, 475);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(811, 325);
+            this.panel2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(211, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "12345679";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(121, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "파일 수 : ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 31);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "파일추가";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.listView2);
+            this.panel7.Location = new System.Drawing.Point(3, 47);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(803, 273);
+            this.panel7.TabIndex = 1;
+            // 
+            // listView2
+            // 
+            this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(0, 0);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(803, 273);
+            this.listView2.TabIndex = 0;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.FileName = "openFileDialog1";
+            this.ofdFile.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdFile_FileOk);
+            // 
+            // ofdFile2
+            // 
+            this.ofdFile2.FileName = "openFileDialog1";
+            this.ofdFile2.Multiselect = true;
+            this.ofdFile2.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdFile2_FileOk);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(306, 248);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(132, 54);
+            this.simpleButton1.TabIndex = 8;
+            this.simpleButton1.Text = "실행";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(11, 8);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "급여제공기간 : ";
+            // 
+            // lblperiod
+            // 
+            this.lblperiod.AutoSize = true;
+            this.lblperiod.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblperiod.Location = new System.Drawing.Point(143, 8);
+            this.lblperiod.Name = "lblperiod";
+            this.lblperiod.Size = new System.Drawing.Size(74, 25);
+            this.lblperiod.TabIndex = 9;
+            this.lblperiod.Text = "기간txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 822);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog ofdFile2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.Label lblperiod;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Eun_payslip/Eun_payslip/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+using System.Collections;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Eun_payslip
+{
+    public partial class Form1 : Form
+    {
+        String _User_list_File; //유저정보갖고있는 File의 주소저장
+        List<string> _All_file = new List<string>(); //모든 선택된 파일의 경로를 갖고있다. 
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 파일선택시 해당 파일의 수급자, 본인부담금데이터를 가져와 ListView에 뿌려준다.
+        /// </summary>
+       
+
+        /// <summary>
+        /// 저장된 File_Path 주소를 List에 뿌려준다.
+        /// </summary>
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofdFile.ShowDialog();
+        }
+
+        private void ofdFile_FileOk(object sender, CancelEventArgs e)
+        {
+            listView1.Items.Clear();
+            listView1.Columns.Clear();
+            string filePath = ofdFile.FileName;
+            _User_list_File = filePath; //파일의 풀경로 저장
+            textBox1.Text = filePath.Split('\\')[filePath.Split('\\').Length - 1]; //파일이름만 읽어온다.
+
+            listView1.Columns.Add("No.", 30);
+            listView1.Columns.Add("수급자", 100);
+            listView1.Columns.Add("본인부담금", 100);
+
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            Excel_Read_User();
+
+        }
+
+
+
+
+        /// <summary>
+        /// 다중파일선택버튼
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ofdFile2.ShowDialog();
+        }
+
+        /// <summary>
+        /// 파일 다중선택시 파일의 주소를 배열로 저장한다.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ofdFile2_FileOk(object sender, CancelEventArgs e)
+        {
+            listView2.Items.Clear();
+            listView2.Columns.Clear();
+            string[] arr = new string[2];
+            ListViewItem itm;
+
+            listView2.View = View.Details;
+            listView2.GridLines = true;
+            listView2.FullRowSelect = true;
+
+            listView2.Columns.Add("No.", 50);
+            listView2.Columns.Add("파일주소", 300);
+
+            for (int i = 0; i < ofdFile2.FileNames.Length; i++)
+            {
+                _All_file.Add(ofdFile2.FileNames[i].ToString()); //모든 파일의 경로를 List에 담는다.(나중에 빼서 하나씩열어서 데이터 저장하기위해서)
+                arr[0] = (i + 1).ToString(); //No. 저장
+                arr[1] = ofdFile2.FileNames[i].ToString(); //File 경로 저장
+                itm = new ListViewItem(arr);
+                listView2.Items.Add(itm);
+            }
+            listView2.Columns[1].Width = -1; //컬럼크기를 텍스트 크기에 맞춰 정렬
+            listView2.Columns[1].Width = -2; //컬럼크기를 텍스트 크기에 맞춰 정렬
+
+        }
+
+        private void Excel_Read_User()
+        {
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook workbook = app.Workbooks.Open(Filename: _User_list_File);
+            Excel.Worksheet worksheet1 = workbook.Worksheets.Item[1]; //Index는 1부터 시작한다.
+            app.Visible = false;
+            Excel.Range range = worksheet1.UsedRange; //사용된 Range를 가져온다. Row와 Column의 정보를 갖고있다. 
+
+
+            String data = "";
+
+            //그리드에 정보를 담는다.
+            string[] arr = new string[3];
+            ListViewItem itm;
+
+            for (int j = 10; j <= range.Rows.Count; j++)
+            {
+                String data_Result = (range.Cells[j, 2] as Excel.Range).Value2.ToString(); //null값이면 빠져나오기 위해
+                if (data_Result != null)
+                {
+                    //data += ((range.Cells[j, 2] as Excel.Range).Value2.ToString() + " ");
+                    arr[0] = (j - 9).ToString(); //No.
+                    arr[1] = ((range.Cells[j, 2] as Excel.Range).Value2.ToString() + " "); //User명
+                    arr[2] = ((range.Cells[j, 8] as Excel.Range).Value2.ToString() + " "); //금액 컬럼 열 변경시 바꿔줘야한다.
+                    itm = new ListViewItem(arr);
+                    listView1.Items.Add(itm);
+                }
+            }
+            lblperiod.Text = (range.Cells[3, 5] as Excel.Range).Value2.ToString(); //급여제공기간
+            //richTextBox1.Text = data;
+
+            DeleteObject(worksheet1);
+            DeleteObject(workbook);
+            app.Quit();
+            DeleteObject(app);
+        }
+
+        /// <summary>
+        /// 개인파일을 열어 정보를 갖고온다. 1.성명, 2.장기요양 3.인정번호, 4.방문요양총금액
+        /// </summary>
+        private void Excel_Detail_Info_Read()
+        {
+            try
+            {
+                Excel.Application app = new Excel.Application();
+
+                /*
+                //for 문으로 리스트 크기만금 돌려서 모두 출력
+                for(int i= 0; i < _All_file.Count; i++)
+                {
+                    MessageBox.Show("실행완료" + i);
+                }
+                */
+
+
+                Excel.Workbook workbook = app.Workbooks.Open(Filename: _All_file[0]);
+                Excel.Worksheet worksheet1 = workbook.Worksheets.Item[1]; //Index는 1부터 시작한다.
+                app.Visible = false;
+                Excel.Range range = worksheet1.UsedRange; //사용된 Range를 가져온다. Row와 Column의 정보를 갖고있다. 
+                String data = "";
+
+                data += ((range.Cells[7, 2] as Excel.Range).Value2.ToString() + " "); //이름
+                data += ((range.Cells[7, 16] as Excel.Range).Value2.ToString() + " "); //장기요양 인정번호
+
+                MessageBox.Show((string)(range.Cells[26, 28] as Excel.Range).Text.ToString());
+
+                for (int j = 21; j < range.Rows.Count; j++) //21은 시작하는열
+                {
+                    //String data_Result = (range.Cells[j, 28] as Excel.Range).Value2.ToString(); //null값이면 빠져나오기 위해
+
+                    // range.Cells[26, 28] as Excel.Range).Value2
+
+
+                    if ((range.Cells[j, 28] as Excel.Range).Value2 != "")
+                    {
+                        int jjjj = 0;
+                        data += ((range.Cells[j, 28] as Excel.Range).Value2.ToString() + " ");
+                    }
+
+                    int gg = 0;
+                }
+                //richTextBox1.Text = data;
+
+                DeleteObject(worksheet1);
+                DeleteObject(workbook);
+                app.Quit();
+                DeleteObject(app);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// 엑셀에 묶인 리소스지워준다.
+        /// 엑셀에 묶인 리소스지워준다.
+        /// </summary>
+        /// <param name="obj"></param>
+        private void DeleteObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("메모리 할당을 해제하는 중 문제가 발생하였습니다." + ex.ToString(), "경고!");
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            Excel_Detail_Info_Read();
+        }
+    }
+}
